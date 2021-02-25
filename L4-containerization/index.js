eae09892bc9ae228ae057cb3b9006e52c7b28dc9
rythae <auxano42@gmail.com>
2021-02-25 10:27:52 +0100

    containerization

---
+++ L4-containerization/index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+var path = __dirname + "/views/";
+
+// Constants
+const PORT = 5000;
+const HOST = "0.0.0.0";
+
+router.use(function (req, res, next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
+});
+
+app.use(express.static(path));
+app.use("/", router);
+
+app.listen(5000, function () {
+  console.log("app listening on port 5000!");
+});
